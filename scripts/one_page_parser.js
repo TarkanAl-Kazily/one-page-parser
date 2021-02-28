281942e41e3b5a65f5865e058e6fb462235afcc8
Tarkan Al-Kazily <tarkan.alkazily@gmail.com>
2021-02-27 18:58:39 -0800

    Form now creates a scene with the given name, grid size, background image

--- scripts/one_page_parser.js
+++ scripts/one_page_parser.js
@@ -30,17 +30,58 @@ class OnePageParserForm extends FormApplication {
 
     // must override - abstract function
     _updateObject(event, formData) {
-        const promiseResult = new Promise(this.parseForm);
+        const promiseResult = new Promise((resolve, reject) => {
+            // The parser logic
+            // Tries to create a new scene from the info in the form.
+            // On success, can call 'resolve' with a helpful message.
+            // On failure, calls 'reject' with an error message.
+            console.log(formData);
+
+            let validData = true;
+
+            if (isNaN(formData.grid)) {
+                ui.notifications.error("Grid Size must be a number");
+                validData = false;
+            }
+            
+            if (formData.grid < 50) {
+                ui.notifications.error("Grid Size must be at least 50");
+                validData = false;
+            }
+
+            if (formData.name == "") {
+                ui.notifications.error("Scene Name must not be empty");
+                validData = false;
+            }
+
+            $.get(formData.img).fail( () => {
+                ui.notifications.error("Background Image file not found");
+                validData = false;
+            });
+
+            $.get(formData.json).fail( () => {
+                ui.notifications.error("JSON Import file not found");
+                validData = false;
+            });
+
+            if (validData) {
+                try {
+                    this.createScene(formData);
+                } catch (error) {
+                    reject(error);
+                }
+            } else {
+                reject("Form data is not valid. See error notifications.");
+            }
+
+            ui.notifications.info("Imported Scene");
+            resolve("Imported Scene");
+        });
         return promiseResult;
     }
 
-    // The parser logic
-    // Tries to create a new scene from the info in the form.
-    // On success, can call 'resolve' with a helpful message.
-    // On failure, calls 'reject' with an error message.
-    async parseForm(resolve, reject) {
-        ui.notifications.error("Not yet implemented");
-        reject("Not yet implemented");
+    createScene(formData) {
+        const scene = Scene.create(formData);
     }
 
 }
